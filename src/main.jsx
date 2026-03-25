1143e0c223ba0ce8f7286673436fd9f0f6b71154
michaelmuirhead <102689372+michaelmuirhead@users.noreply.github.com>
2026-03-24 22:40:28 -0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import PaycheckPlanner from './PaycheckPlanner.jsx'
+import MaverickFinance from './PaycheckPlanner.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PaycheckPlanner />
+    <MaverickFinance />
   </React.StrictMode>,
 )
