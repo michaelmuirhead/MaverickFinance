0f0874a9182656521d9a114875bd0d0fa2db7bdf
michaelmuirhead <102689372+michaelmuirhead@users.noreply.github.com>
2026-03-25 06:35:51 -0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import MaverickFinance from './PaycheckPlanner.jsx'
+import MaverickFinance from './MaverickFinance.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
